2dd6c3d7b25d1d149ceb0582c3710f03a502845c
joaodedeusrsfilho <joaodedeusrsfilho@gmail.com>
2023-04-06 13:07:30 -0300

    add file

--- Exercicio22.java
+++ Exercicio22.java
@@ -5,25 +5,24 @@ import java.util.Scanner;
 public class Exercicio22 {
 
 	public static void main(String[] args) {
-		/*Criar um vetorA com 10 elementos inteiros. Desenvolver um
-		programa que verifique se "todos" os elementos do vetor A
-		são pares. 
 		
-		Se pelo menos um elemento do vetor não for
-		par, o processo de repetição para de percorrer os elementos
-		do vetor e deve ser encerrado*/
-		
+//		 Criar um vetorA com 10 elementos inteiros. Desenvolver um programa que
+//		 verifique se "todos" os elementos do vetor A são pares.
+//		  
+//		 Se pelo menos um elemento do vetor não for par, o processo de repetição para
+//		 de percorrer os elementos do vetor e deve ser encerrado
+		 
+
 		Scanner ler = new Scanner(System.in);
-		
-		int[] vetorA = new int[4];
-		
-		
-		for(int x=0; x< vetorA.length;x++) {
-			System.out.println("Entre com um numero para a posicao: "+(x+1));
+
+		int[] vetorA = new int[20];
+
+		for (int x = 0; x < vetorA.length; x++) {
+			System.out.println("Entre com um numero para a posicao: " + (x + 1));
 			vetorA[x] = ler.nextInt();
-			
-			if(vetorA[x] % 2 !=0) {
-				break;//para parar o laço
+
+			if (vetorA[x] % 2 != 0) {
+				break;// para parar o laço
 			}
 		}
 	}
